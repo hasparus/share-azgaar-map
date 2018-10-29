3ddf5bca8eb78996def56e84449be50542c4ead0
hasparus <hasparus@gmail.com>
2018-10-29 12:08:38 +0100

    Change url

--- map-share-common/urls.ts
+++ map-share-common/urls.ts
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 } else {
   urls = {
     GENERATOR_URL: 'https://hasparus.github.io/Fantasy-Map-Generator/',
-    SERVICE_URL: 'http://localhost:3000',
+    SERVICE_URL: 'https://map-share-service.now.sh',
   };
 }
 
