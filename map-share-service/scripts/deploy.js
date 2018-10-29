5bd8cae67397643431d3b68bb44f47e837fa62b0
hasparus <hasparus@gmail.com>
2018-10-29 12:41:26 +0100

    Move now config out of pkg.json

--- map-share-service/scripts/deploy.js
+++ map-share-service/scripts/deploy.js
@@ -4,10 +4,9 @@ const { deployDir } = require('./constants');
 
 function deploy() {
   exec(`
-    now --public ${deployDir}
+    now --public ${deployDir} -e DROPBOX_ACCESS_TOKEN=@dropbox_access_token -e NODE_ENV=production
     now alias
     now rm $npm_package_name --safe --yes
-    rm -rf ${deployDir}
   `);
 }
 
