4c03b2161743d1ac5f2db4ae65e24e096518bf9b
hasparus <hasparus@gmail.com>
2018-11-07 16:04:45 +0100

    Remove underline and fix padding

--- map-share-frontend/src/Footer.tsx
+++ map-share-frontend/src/Footer.tsx
@@ -18,6 +18,11 @@ const footerItemStyle = css`
   font-size: 0.6em;
   color: #333;
   text-decoration: none;
+  display: flex;
+
+  > * {
+    color: inherit;
+  }
 `;
 
 export const Footer: Component = (attrs, children) => (
